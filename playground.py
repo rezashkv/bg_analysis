60f38395048666ecd188660da33bd49556d94426
Reza Shirkavand <103207797+rezashkv@users.noreply.github.com>
2023-12-01 10:25:35 -0500

    Delete playground.py

--- playground.py
+++
@@ -1,7 +0,0 @@
-import os
-
-path = "/fs/nexus-scratch/rezashkv/research/data/bg_challenge/test/original/val"
-
-#iterate over all folders in path and print the number of files in each folder
-for folder in os.listdir(path):
-    print(folder, len(os.listdir(os.path.join(path, folder))))
